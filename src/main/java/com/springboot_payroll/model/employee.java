08c855ff87d607d59dd436e976e3a571d820752b
Aarti Singh <aartisingh@Aartis-MacBook-Pro.local>
2022-09-15 16:12:25 -0700

    new tests

--- src/main/java/com/springboot_payroll/model/employee.java
+++ src/main/java/com/springboot_payroll/model/employee.java
@@ -7,13 +7,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 @Entity
 @Table(name="employees")
 public class employee {
-	
+
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
--- src/test/java/com/springboot_payroll/SpringbootPayrollApplicationTests.java
+++ src/test/java/com/springboot_payroll/SpringbootPayrollApplicationTests.java
@@ -1,13 +1,23 @@
 package com.springboot_payroll;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import com.springboot_payroll.model.employee;
+import com.springboot_payroll.repo.employeeRepo;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SpringbootPayrollApplicationTests {
-
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads()
+	{
+		
 	}
-
+	
 }
+
